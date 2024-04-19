fc8c957128b5ac61d4d15395fe880f45e44a2cb0
olael94 <104049815+olael94@users.noreply.github.com>
2024-04-19 03:05:19 -0600

    added comments to nextCOnfigProd for basePath deployment in different platforms. I also added the pointer hover for my dropdown menu.

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,7 @@ const env = process.env.NODE_ENV || 'development';
 /** @type {import('next').NextConfig} */
 const nextConfigProd = {
   output: 'export',
+  /* Make sure to change the basePath according to the platform you are using to deploy */
   basePath: '',
   images: {
     loader: 'custom',
